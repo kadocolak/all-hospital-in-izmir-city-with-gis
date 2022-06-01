b7735db3e3fa13492b4bc4e5f086c03bdf8740b2
Kadir ÇOLAK <kadircolak1999@gmail.com>
2022-06-01 20:43:35 +0300

    Update izmirData.js
    js code minifiy and common constructs run within a single function

--- izmirData.js
+++ izmirData.js
@@ -5,90 +5,28 @@ var map = new google.maps.Map(document.getElementById("map"), {
    disableDefaultUI: !0, scaleControl: !0, fullscreenControl: !0,
    clickableIcons: false
 });
-  $.getJSON("https://openapi.izmir.bel.tr/api/ibb/cbs/agizvedissagligimerkezleri", function (data) {
-    for(var i=0;i<Object.keys(data).length;i++)
-    {
-       var lat = data.onemliyer[i].ENLEM;
-       var lng = data.onemliyer[i].BOYLAM;
-       var title = data.onemliyer[i].ADI;
-       marker = new google.maps.Marker({
-	   position: { lat: lat , lng: lng },
-           map: map,
-	       title: title,
-		   icon: "https://cdn3.iconfinder.com/data/icons/clinical-3/96/dental-32.png"
-       });
-    }
- });
- $.getJSON("https://openapi.izmir.bel.tr/api/ibb/cbs/anacocuksagligimerkezleri", function (data) {
-    for(var i=0;i<Object.keys(data).length;i++)
-    {
-       var lat = data.onemliyer[i].ENLEM;
-       var lng = data.onemliyer[i].BOYLAM;
-       var title = data.onemliyer[i].ADI;
-       marker = new google.maps.Marker({
-	   position: { lat: lat , lng: lng },
-           map: map,
-	       title: title,
-		   icon: "https://cdn3.iconfinder.com/data/icons/family-member-flat-happy-family-day/512/Son-32.png"
-       });
-    }
- });
- $.getJSON("https://openapi.izmir.bel.tr/api/ibb/cbs/hastaneler", function (data) {
-    for(var i=0;i<Object.keys(data).length;i++)
-    {
-       var lat = data.onemliyer[i].ENLEM;
-       var lng = data.onemliyer[i].BOYLAM;
-       var title = data.onemliyer[i].ADI;
-       marker = new google.maps.Marker({
-	   position: { lat: lat , lng: lng },
-           map: map,
-	       title: title,
-		   icon: "https://cdn2.iconfinder.com/data/icons/covid-19-2/64/30-Doctor-32.png"
-       });
-    }
- });
- $.getJSON("https://openapi.izmir.bel.tr/api/ibb/cbs/kanmerkezleri", function (data) {
-    for(var i=0;i<Object.keys(data).length;i++)
-    {
-       var lat = data.onemliyer[i].ENLEM;
-       var lng = data.onemliyer[i].BOYLAM;
-       var title = data.onemliyer[i].ADI;
-       marker = new google.maps.Marker({
-	   position: { lat: lat , lng: lng },
-           map: map,
-	       title: title,
-		   icon: "https://cdn3.iconfinder.com/data/icons/clinical-3/96/blood-32.png"
-       });
-    }
- });
-  $.getJSON("https://openapi.izmir.bel.tr/api/ibb/cbs/laboratuvarlar", function (data) {
-    for(var i=0;i<Object.keys(data).length;i++)
-    {
-       var lat = data.onemliyer[i].ENLEM;
-       var lng = data.onemliyer[i].BOYLAM;
-       var title = data.onemliyer[i].ADI;
-       marker = new google.maps.Marker({
-	   position: { lat: lat , lng: lng },
-           map: map,
-	       title: title,
-		   icon: "https://cdn3.iconfinder.com/data/icons/education-209/64/tube-lab-science-school-32.png"
-       });
-    }
- });
-  $.getJSON("https://openapi.izmir.bel.tr/api/ibb/cbs/veterinerlikler", function (data) {
-    for(var i=0;i<Object.keys(data).length;i++)
-    {
-       var lat = data.onemliyer[i].ENLEM;
-       var lng = data.onemliyer[i].BOYLAM;
-       var title = data.onemliyer[i].ADI;
-       marker = new google.maps.Marker({
-	   position: { lat: lat , lng: lng },
-           map: map,
-	       title: title,
-		   icon: "https://cdn3.iconfinder.com/data/icons/animals-105/150/icon_animal_gato-32.png"
-       });
-    }
- });
+var apiStartLink = "https://openapi.izmir.bel.tr/api/ibb/cbs/";
+var iconStartLink = "https://cdn3.iconfinder.com/data/icons/";
+var jsonList = [[apiStartLink.toString()+"agizvedissagligimerkezleri"],[apiStartLink.toString()+"anacocuksagligimerkezleri"],[apiStartLink.toString()+"hastaneler"],
+[apiStartLink.toString()+"kanmerkezleri"],[apiStartLink.toString()+"laboratuvarlar"],[apiStartLink.toString()+"veterinerlikler"]];
+var iconList = [[iconStartLink.toString()+"clinical-3/96/dental-32.png"],[iconStartLink.toString()+"family-member-flat-happy-family-day/512/Son-32.png"],[iconStartLink.toString()+"covid-19-2/64/30-Doctor-32.png"],
+[iconStartLink.toString()+"clinical-3/96/blood-32.png"],[iconStartLink.toString()+"education-209/64/tube-lab-science-school-32.png"],[iconStartLink.toString()+"animals-105/150/icon_animal_gato-32.png"]];
+jsonList.forEach(arrayLoop);
+var lat,lng,title;
+function arrayLoop(item,index)
+{
+	$.getJSON(item.toString(),function(data){
+		for(var i=0;i<Object.keys(data).length;i++)
+        {
+		   lat = data.onemliyer[i].ENLEM;
+           lng = data.onemliyer[i].BOYLAM;
+           title = data.onemliyer[i].ADI;
+		   marker = new google.maps.Marker({
+	       position: { lat: lat , lng: lng },
+           map: map, title: title, icon: iconList[index].toString()
+           });
+	    }
+	});
+}
 }
 initMap();
-
